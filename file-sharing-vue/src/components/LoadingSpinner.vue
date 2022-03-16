7bd1b015021b16111bf29a17a2c4c2c4b254e80b
joshuaboud <joshuaboud@hotmail.com>
2022-03-16 13:27:42 -0300

    add loading spinner component

---
+++ file-sharing-vue/src/components/LoadingSpinner.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="loading-spinner border-neutral-200 dark:border-t-neutral-200 dark:border-neutral-500 border-t-neutral-500 rounded-full"></div>
+</template>
+
+<style>
+.loading-spinner {
+	border-width: 0.2rem;
+	animation: spin 2s linear infinite;
+	display: inline-block;
+}
+
+@keyframes spin {
+	0% {
+		transform: rotate(0deg);
+	}
+	100% {
+		transform: rotate(360deg);
+	}
+}
+</style>
