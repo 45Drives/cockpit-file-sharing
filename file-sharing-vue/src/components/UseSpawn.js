aa67c31b1ba7d29f8b8a88b97e77de92ded1a464
joshuaboud <joshuaboud@hotmail.com>
2022-03-16 13:27:31 -0300

    wrapper for cockpit.spawn

---
+++ file-sharing-vue/src/components/UseSpawn.js
@@ -0,0 +1,73 @@
+import { reactive, watch } from 'vue';
+
+if (typeof cockpit === 'undefined') {
+	window.cockpit = {
+		spawn: (argv, opts) => {
+			console.log("spawn", argv)
+			return new Promise((resolve, reject) => resolve(""));
+		}
+	}
+}
+
+/**
+ * 
+ * @param {string[]} argv
+ * @param {Object} opts
+ * @param {string} opts.superuser
+ * @param {string} opts.host
+ * @param {string} opts.directory
+ * @param {boolean} opts.binary
+ * @param {boolean} opts.promise
+ * @param {'out'|'err'} stderr
+ * @returns 
+ */
+export default function useSpawn(argv = [], opts = {}, stderr = 'out') {
+	const state = reactive({
+		loading: false,
+		status: 0,
+		stdout: '',
+		stderr: '',
+	});
+
+	const usePromise = Boolean(opts.promise);
+
+	if (!opts.superuser) opts.superuser = 'require';
+	opts.err = stderr === 'out' ? 'out' : 'message';
+
+	async function execute() {
+		state.loading = true;
+		state.status = 0;
+		state.stdout = '';
+		state.stderr = '';
+
+		cockpit.spawn(argv, opts)
+			.then((_stdout, _stderr) => {
+				state.stdout = _stdout;
+				state.stderr = _stderr;
+			})
+			.catch((ex, _stderr) => {
+				state.stderr = _stderr ?? ex.message;
+				state.status = ex.exit_status ?? -1;
+			})
+			.finally(() => {
+				state.loading = false;
+			});
+	}
+
+	execute();
+
+	if (usePromise) {
+		return new Promise((resolve, reject) => {
+			watch(state, () => {
+				if (!state.loading) {
+					if (state.status !== 0)
+						reject({ ...state });
+					else
+						resolve({ ...state });
+				}
+			});
+		});
+	} else {
+		return state;
+	}
+}
