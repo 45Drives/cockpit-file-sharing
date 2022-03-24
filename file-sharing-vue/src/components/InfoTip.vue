f12c287f4e1181eab230e79e0a3889a62974d16e
joshuaboud <joshuaboud@hotmail.com>
2022-03-24 11:59:32 -0300

    add info tip

---
+++ file-sharing-vue/src/components/InfoTip.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="ml-1 relative overflow-visible inline-block">
+		<QuestionMarkCircleIcon class="text-gray-500 w-5 h-5" @mouseover="show=true" @mouseleave="show=false" />
+		<div class="absolute bg-neutral-500 text-gray-50 rounded-lg p-2 z-50 w-60" v-if="show" @mouseover="show=true" @mouseleave="show=false">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { QuestionMarkCircleIcon } from "@heroicons/vue/solid";
+export default {
+	setup() {
+		const show = ref(false);
+
+		return { show };
+	},
+	components: {
+		QuestionMarkCircleIcon,
+	}
+}
+</script>
