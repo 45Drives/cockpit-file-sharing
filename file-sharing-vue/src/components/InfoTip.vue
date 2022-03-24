166c8c43b087d4ab82e666dee25acb0d77a5db0e
joshuaboud <joshuaboud@hotmail.com>
2022-03-24 14:25:08 -0300

    improve responsiveness

--- file-sharing-vue/src/components/InfoTip.vue
+++ file-sharing-vue/src/components/InfoTip.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="ml-1 relative overflow-visible inline-block">
-		<QuestionMarkCircleIcon class="text-gray-500 w-5 h-5" @mouseover="show=true" @mouseleave="show=false" />
-		<div class="absolute bg-neutral-500 text-gray-50 rounded-lg p-2 z-50 w-60" v-if="show" @mouseover="show=true" @mouseleave="show=false">
+	<div class="ml-1 relative overflow-visible inline-block" @click="show = !show" @mouseenter="show=true" @mouseleave="show=false" >
+		<QuestionMarkCircleIcon class="text-gray-500 w-5 h-5" />
+		<div class="absolute bg-neutral-500 text-gray-50 rounded-lg p-2 z-50 w-60" v-if="show" >
 			<slot />
 		</div>
 	</div>
