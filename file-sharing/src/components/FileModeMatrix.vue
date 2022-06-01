26cf08ead9ef536c326a28e0198eff5fb3049518
joshuaboud <joshuaboud@hotmail.com>
2022-06-01 13:31:38 -0300

    rewrite logic of mode matrix to use computed values

--- file-sharing/src/components/FileModeMatrix.vue
+++ file-sharing/src/components/FileModeMatrix.vue
@@ -1,98 +1,77 @@
 <template>
-	<div class="space-y-1">
-		<div class="grid grid-cols-4 gap-2 justify-items-center">
-			<label class="justify-self-start block text-sm font-medium"></label>
-			<label class="text-label">Read</label>
-			<label class="text-label">Write</label>
-			<label class="text-label">Execute</label>
+	<div class="inline-grid grid-cols-[2fr_1fr_1fr_1fr] gap-2 justify-items-center">
+		<label class="justify-self-start block text-sm font-medium"></label>
+		<label class="text-label">Read</label>
+		<label class="text-label">Write</label>
+		<label class="text-label">Execute</label>
 
-			<label class="justify-self-start text-label">Owner</label>
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.owner.read" />
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.owner.write" />
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.owner.execute" />
+		<label class="justify-self-start text-label">Owner</label>
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.owner.read" />
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.owner.write" />
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.owner.execute" />
 
-			<label class="justify-self-start text-label">Group</label>
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.group.read" />
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.group.write" />
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.group.execute" />
+		<label class="justify-self-start text-label">Group</label>
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.group.read" />
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.group.write" />
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.group.execute" />
 
-			<label class="justify-self-start text-label">Other</label>
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.other.read" />
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.other.write" />
-			<input @change="updateValue" type="checkbox" class="input-checkbox" v-model="modeMatrix.other.execute" />
-		</div>
+		<label class="justify-self-start text-label">Other</label>
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.other.read" />
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.other.write" />
+		<input type="checkbox" class="input-checkbox" v-model="modeMatrix.other.execute" />
 
-		<div>
-			<label class="text-label">Result</label>
-			<span
-				class="ml-8 font-mono font-medium whitespace-nowrap"
-			>{{ modeStr }}</span>
-		</div>
+		<label class="justify-self-start text-label">Mode</label>
+		<span class="col-span-3 font-mono font-medium whitespace-nowrap">{{ modeStr }}</span>
 	</div>
 </template>
 
 <script>
-import { ref, reactive, watch } from 'vue';
+import { reactive, computed } from 'vue';
 export default {
 	props: {
 		modelValue: Number,
 	},
 	setup(props, { emit }) {
-		const modeStr = ref("");
+		const modeStr = computed(
+			() =>
+				(props.modelValue & 0b100000000 ? 'r' : '-')
+				+ (props.modelValue & 0b010000000 ? 'w' : '-')
+				+ (props.modelValue & 0b001000000 ? 'x' : '-')
+				+ (props.modelValue & 0b000100000 ? 'r' : '-')
+				+ (props.modelValue & 0b000010000 ? 'w' : '-')
+				+ (props.modelValue & 0b000001000 ? 'x' : '-')
+				+ (props.modelValue & 0b000000100 ? 'r' : '-')
+				+ (props.modelValue & 0b000000010 ? 'w' : '-')
+				+ (props.modelValue & 0b000000001 ? 'x' : '-')
+				+ ` (${props.modelValue.toString(8).padStart(3, '0')})`
+		);
+
+		const computedBit = (mask, getter) => computed({
+			get: () => getter() & mask ? true : false,
+			set: (value) => emit('update:modelValue', value ? (props.modelValue | mask) : (props.modelValue & (~mask))),
+		})
+
 		const modeMatrix = reactive({
-			owner: { read: true, write: true, execute: true },
-			group: { read: true, write: false, execute: true },
-			other: { read: true, write: false, execute: true },
+			other: {
+				execute: computedBit(0b000000001, () => props.modelValue),
+				write: computedBit(0b000000010, () => props.modelValue),
+				read: computedBit(0b000000100, () => props.modelValue),
+			},
+			group: {
+				execute: computedBit(0b000001000, () => props.modelValue),
+				write: computedBit(0b000010000, () => props.modelValue),
+				read: computedBit(0b000100000, () => props.modelValue),
+			},
+			owner: {
+				execute: computedBit(0b001000000, () => props.modelValue),
+				write: computedBit(0b010000000, () => props.modelValue),
+				read: computedBit(0b100000000, () => props.modelValue),
+			},
 		});
-
-		const updateValue = () => {
-			const mode =
-				(modeMatrix.other.execute ? 0b000000001 : 0)
-				| (modeMatrix.other.write ? 0b000000010 : 0)
-				| (modeMatrix.other.read ? 0b000000100 : 0)
-				| (modeMatrix.group.execute ? 0b000001000 : 0)
-				| (modeMatrix.group.write ? 0b000010000 : 0)
-				| (modeMatrix.group.read ? 0b000100000 : 0)
-				| (modeMatrix.owner.execute ? 0b001000000 : 0)
-				| (modeMatrix.owner.write ? 0b010000000 : 0)
-				| (modeMatrix.owner.read ? 0b100000000 : 0);
-			emit('update:modelValue', mode);
-		}
-
-		watch(modeMatrix, (current, old) => {
-			modeStr.value =
-				(modeMatrix.owner.read ? 'r' : '-')
-				+ (modeMatrix.owner.write ? 'w' : '-')
-				+ (modeMatrix.owner.execute ? 'x' : '-')
-				+ (modeMatrix.group.read ? 'r' : '-')
-				+ (modeMatrix.group.write ? 'w' : '-')
-				+ (modeMatrix.group.execute ? 'x' : '-')
-				+ (modeMatrix.other.read ? 'r' : '-')
-				+ (modeMatrix.other.write ? 'w' : '-')
-				+ (modeMatrix.other.execute ? 'x' : '-')
-				+ ` (${props.modelValue.toString(8).padStart(3, '0')})`;
-		}, { deep: true });
-
-		watch(() => props.modelValue, (current, old) => {
-			const other = {};
-			const group = {};
-			const owner = {};
-			other.execute = props.modelValue & 0b000000001 ? true : false;
-			other.write = props.modelValue & 0b000000010 ? true : false;
-			other.read = props.modelValue & 0b000000100 ? true : false;
-			group.execute = props.modelValue & 0b000001000 ? true : false;
-			group.write = props.modelValue & 0b000010000 ? true : false;
-			group.read = props.modelValue & 0b000100000 ? true : false;
-			owner.execute = props.modelValue & 0b001000000 ? true : false;
-			owner.write = props.modelValue & 0b010000000 ? true : false;
-			owner.read = props.modelValue & 0b100000000 ? true : false;
-			Object.assign(modeMatrix, { other, group, owner });
-		}, { deep: true, immediate: true })
 
 		return {
 			modeMatrix,
 			modeStr,
-			updateValue,
 		}
 	},
 	emits: [
