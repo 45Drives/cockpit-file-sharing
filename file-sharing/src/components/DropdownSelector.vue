ba50d9303b33e2cabf47e1c30a154034e5bf720b
joshuaboud <joshuaboud@hotmail.com>
2022-06-01 13:31:17 -0300

    fix styling of select

--- file-sharing/src/components/DropdownSelector.vue
+++ file-sharing/src/components/DropdownSelector.vue
@@ -18,7 +18,7 @@ If not, see <https://www.gnu.org/licenses/>.
 <template>
 	<select
 		@change="select($event)"
-		class="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-500 sm:text-sm rounded-md dark:bg-neutral-800"
+		class="input-textlike"
 	>
 		<option value disabled selected hidden>{{ placeholder }}</option>
 		<option v-for="(option, index) in options" :value="index">{{ option?.pretty ?? option }}</option>
