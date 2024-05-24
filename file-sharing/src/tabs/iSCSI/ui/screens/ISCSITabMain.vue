cf4e7708beccf89cef9318bdb2b538aea8363999
Brandon Kelly <brkelly@protocase.com>
2024-05-24 11:13:10 -0300

    moved tables to their own components

--- file-sharing/src/tabs/iSCSI/ui/screens/ISCSITabMain.vue
+++ file-sharing/src/tabs/iSCSI/ui/screens/ISCSITabMain.vue
@@ -1,126 +1,27 @@
 <template>
     <CenteredCardColumn>
-        <CardContainer>
-            <template v-slot:header>
-                Devices
-            </template>
-			<div class="overflow-hidden"
-				:style="{ 'max-height': showAddDevice ? '1500px' : '0', transition: showAddDevice ? 'max-height 0.5s ease-in' : 'max-height 0.5s ease-out' }">
-				<div class="card">
-					<VirtualDeviceEditor :existing-devices="virtualDevices" @close-editor="() => {showAddDevice = false; refreshDevices()}"/>
-				</div>
-			</div>
-			<div class="card">
-				<div class="sm:shadow sm:rounded-lg sm:border sm:border-default overflow-hidden">
-					<Table emptyText="No devices. Click '+' to add one." noScroll noHeader
-						class="!border-none !shadow-none">
-						<template #thead>
-							<tr>
-								<th scope="col">Device Name</th>
-								<th scope="col">File Path</th>
-								<th scope="col">Block Size</th>
-								<th scope="col">Type</th>
-								<th scope="col" class="flex flex-row justify-end">
-									<span class="sr-only">Delete</span>
-									<button @click="showAddDevice = !showAddDevice">
-										<PlusIcon class="size-icon icon-default" />
-									</button>
-								</th>
-							</tr>
-						</template>
-						<template #tbody>
-							<VirtualDeviceEntry v-for="(device, index) in virtualDevices" :key="index" :device="device" @deleteDevice="actions.refreshDevices"/>
-						</template>
-					</Table>
-				</div>
-			</div>
-        </CardContainer>
+        <VirtualDeviceTable/>
 
-		<CardContainer>
-			<template v-slot:header>
-                Targets
-            </template>
-			<div class="overflow-hidden"
-				:style="{ 'max-height': showAddTarget ? '1500px' : '0', transition: showAddTarget ? 'max-height 0.5s ease-in' : 'max-height 0.5s ease-out' }">
-				<div class="card">
-					<TargetEditor :existing-targets="targets" @close-editor="() => {showAddTarget = false; refreshTargets()}"/>
-				</div>
-			</div>
-			<div class="card">
-				<div class="sm:shadow sm:rounded-lg sm:border sm:border-default overflow-hidden">
-					<Table emptyText="No targets. Click '+' to add one." noScroll noHeader
-						class="!border-none !shadow-none">
-						<template #thead>
-							<tr>
-								<th scope="col">Target Name</th>
-								<th scope="col" class="flex flex-row justify-end">
-									<span class="sr-only">Edit/Delete</span>
-									<button @click="showAddTarget = !showAddTarget">
-										<PlusIcon class="size-icon icon-default" />
-									</button>
-								</th>
-							</tr>
-						</template>
-						<template #tbody>
-							<TargetEntry v-for="(target, index) in targets" :key="index" :target="target" @deleteTarget="actions.refreshTargets"/>
-						</template>
-					</Table>
-				</div>
-			</div>
-		</CardContainer>
+		<TargetTable/>
     </CenteredCardColumn>
 </template>
 
 <script setup lang="ts">
-	import { CenteredCardColumn, CardContainer, wrapActions } from "@45drives/houston-common-ui";
-	import { provide, ref } from "vue";
-	import { VirtualDevice } from "../types/VirtualDevice";
-	import Table from "./Table.vue";
-	import { PlusIcon } from "@heroicons/vue/24/solid";
-	import VirtualDeviceEntry from "./virtualDevice/VirtualDeviceEntry.vue";
-	import VirtualDeviceEditor from "./virtualDevice/VirtualDeviceEditor.vue";
-	import TargetEntry from "./target/TargetEntry.vue"
+	import { CenteredCardColumn } from "@45drives/houston-common-ui";
+	import { provide } from "vue";
 	import { ISCSIDriverSingleServer } from "../types/ISCSIDriverSingleServer";
 	import { getServer } from "@45drives/houston-common-lib";
-	import type { Target } from "../types/Target";
-import TargetEditor from "./target/TargetEditor.vue";
-
-	const virtualDevices = ref<VirtualDevice[]>([]);
-	const targets = ref<Target[]>([]);
+	import VirtualDeviceTable from "./virtualDevice/VirtualDeviceTable.vue";
+	import TargetTable from "./target/TargetTable.vue";
 
 	const iSCSIDriver = createISCSIDriver();
 
-	const showAddDevice = ref(false);
-
-	const showAddTarget = ref(false);
-
 	provide('iSCSIDriver', iSCSIDriver);
-
-	refreshDevices();
-	refreshTargets();
 
 	function createISCSIDriver(){
 		return getServer().map((server) => {
 			return new ISCSIDriverSingleServer(server);
 		})
 	}
-
-	function refreshDevices() {
-		return iSCSIDriver.andThen((driver) => {
-			return driver.getVirtualDevices().map((devices) => {
-				virtualDevices.value = devices;
-			});
-		});
-	}
-
-	function refreshTargets() {
-		return iSCSIDriver.andThen((driver) => {
-			return driver.getTargets().map((targetList) => {
-				targets.value = targetList;
-			})
-		});
-	}
-
-	const actions = wrapActions({createISCSIDriver, refreshDevices: refreshDevices, refreshTargets: refreshTargets})
 
 </script>
---
+++ file-sharing/src/tabs/iSCSI/ui/screens/target/TargetEditor.vue
@@ -0,0 +1,93 @@
+<template> 
+    <CardContainer>
+        <div
+            v-if="tempTarget"
+            class="space-y-content"
+        >
+            <div class="space-y-content text-base">
+                <div
+                    class="text-header"
+                >New Target</div>
+            </div>
+
+            <div>
+                <InputField
+                        :placeholder="'A unique name for your target'"
+                        :validator="targetNameValidator"
+                        v-model="tempTarget.name"
+                    >
+                    {{ ("Target Name") }}
+                </InputField>
+            </div>
+        </div>
+
+        <template v-slot:footer>
+            <div class="button-group-row justify-end grow">
+                <button
+                    class="btn btn-secondary"
+                    @click="handleClose"
+                >{{ ("Cancel") }}</button>
+                <button
+                    class="btn btn-primary"
+                    @click="actions.createTarget"
+                    :disabled="!modified"
+                >{{ ("Create") }}</button>
+            </div>
+        </template>
+    </CardContainer>
+</template>
+
+<script setup lang="ts">
+import { CardContainer, InputField, useTempObjectStaging, wrapActions, type InputValidator } from '@45drives/houston-common-ui';
+import type { ResultAsync } from 'neverthrow';
+import { inject, ref } from 'vue';
+import { Target } from '../../types/Target';
+import { ProcessError } from '@45drives/houston-common-lib';
+import type { ISCSIDriver } from '../../types/ISCSIDriver';
+
+    const props = defineProps<{existingTargets: Target[]}>();
+
+    const emit = defineEmits(['closeEditor']);
+
+    const newTarget = ref<Target>(new Target(""));
+
+    const { tempObject: tempTarget, modified, resetChanges } = useTempObjectStaging(newTarget);
+
+    const driver = inject<ResultAsync<ISCSIDriver, ProcessError>>("iSCSIDriver");
+
+    if (driver === undefined) {
+		throw new Error("iSCSI Driver is null");
+	}
+
+    const handleClose = () => {
+        emit("closeEditor");
+        resetChanges();
+    }
+
+    const createTarget = () => {
+        return driver.andThen((driver) => driver.createTarget(tempTarget.value))
+                        .map(() => handleClose())
+                        .mapErr((error) => new ProcessError(`Unable to create target ${tempTarget.value.name}: ${error.message}`))
+    }
+
+    const actions = wrapActions({createTarget});
+
+    const targetNameValidator: InputValidator = (name: string) => {
+        if (!name) {
+            return {
+                type: "error",
+                message: ("A target name is required."),
+            }
+        }
+
+        if (props.existingTargets.find((target) => (target.name === name)) !== undefined) {
+            return {
+                type: "error",
+                message: ("A target with this name already exists."),
+            }
+        }
+
+        return;
+    }
+    
+</script>
