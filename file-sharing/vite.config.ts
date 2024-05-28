7da8e07910518279619686d304141790bc0f19e5
joshuaboud <joshuaboud@hotmail.com>
2024-05-28 11:43:20 -0300

    include sourcemap in build

--- file-sharing/vite.config.ts
+++ file-sharing/vite.config.ts
@@ -1,21 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), VueDevTools()],
-  base: './',
+  base: "./",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    dedupe: ['vue'],
+    dedupe: ["vue"],
   },
   build: {
-    target: ['chrome87', 'edge88', 'firefox78', 'safari14'],
-  }
-})
+    target: ["chrome87", "edge88", "firefox78", "safari14"],
+    sourcemap: true,
+  },
+});
