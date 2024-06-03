90cb987fc6253bb97ea06bcab637e4618a125afd
joshuaboud <joshuaboud@hotmail.com>
2024-06-03 17:27:55 -0300

    implement import/export of config

--- file-sharing/src/tabs/samba/ui/SambaTabMain.vue
+++ file-sharing/src/tabs/samba/ui/SambaTabMain.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { getServer } from "@45drives/houston-common-lib";
+import { getServer, Download } from "@45drives/houston-common-lib";
 import {
   CenteredCardColumn,
   Notification,
   pushNotification,
   wrapActions,
+  CardContainer,
+  FileUploadButton,
+  assertConfirm,
 } from "@45drives/houston-common-ui";
 
 import GlobalConfigEditor from "./GlobalConfigEditor.vue";
@@ -16,18 +19,19 @@ import { clusterScopeInjectionKey, cephClientNameInjectionKey } from "@/common/i
 
 import { getSambaManager } from "@/tabs/samba/samba-manager";
 
+import { ResultAsync, ok, err } from "neverthrow";
+
 const _ = cockpit.gettext;
 
 provide(clusterScopeInjectionKey, getClusterScope("ctdb"));
 const cephClientName = ref("client.samba");
 provide(cephClientNameInjectionKey, cephClientName);
 
-const sambaManager = getServer().map((server) => getSambaManager(server));
+const server = getServer();
+
+const sambaManager = server.map((server) => getSambaManager(server));
 
 const defaultSmbConfPath = "/etc/samba/smb.conf";
-
-const fixSmbConfIncludeRegistry = () =>
-  sambaManager.andThen((sm) => sm.patchSmbConfIncludeRegistry(defaultSmbConfPath));
 
 const checkSambaConf = () =>
   sambaManager
@@ -49,9 +53,47 @@ const checkSambaConf = () =>
       }
     });
 
+const fixSmbConfIncludeRegistry = () =>
+  sambaManager.andThen((sm) => sm.patchSmbConfIncludeRegistry(defaultSmbConfPath));
+
+const uploadRef = ref<InstanceType<typeof FileUploadButton> | null>(null);
+
+const importConfig = () =>
+  assertConfirm({
+    header: _("Overwrite current configuration?"),
+    body: _("Any sections in uploaded file will overwrite current configuration sections."),
+    dangerous: true,
+  })
+    .andThen(() => {
+      if (uploadRef.value === null) {
+        return err(new Error("uploadRef was null!"));
+      }
+      return uploadRef.value.getUpload();
+    })
+    .andThen(([file]) => (file === undefined ? err(new Error("No file given")) : ok(file)))
+    .andThen((file) => ResultAsync.fromSafePromise(file.text()))
+    .andThen((newConfigContents) =>
+      sambaManager.andThen((sm) => sm.importConfig(newConfigContents))
+    );
+
+const exportConfig = () =>
+  sambaManager
+    .andThen((sm) => sm.exportConfig())
+    .map((config) =>
+      Download.content(
+        [config],
+        `cockpit-file-sharing_samba_exported_${new Date()
+          .toISOString()
+          .replace(/:/g, "-")
+          .replace(/T/, "_")}.conf`
+      )
+    );
+
 const actions = wrapActions({
-  fixSmbConfIncludeRegistry,
   checkSambaConf,
+  fixSmbConfIncludeRegistry,
+  importConfig,
+  exportConfig,
 });
 
 onMounted(actions.checkSambaConf);
@@ -61,7 +103,19 @@ onMounted(actions.checkSambaConf);
   <CenteredCardColumn>
     <GlobalConfigEditor />
     <ShareListView />
+    <CardContainer>
+      <template #header>
+        {{ _("Import/Export Config") }}
+      </template>
+      <div class="button-group-row">
+        <button class="btn btn-primary" @click="actions.importConfig">
+          {{ _("Import") }}
+        </button>
+        <button class="btn btn-primary" @click="actions.exportConfig">
+          {{ _("Export") }}
+        </button>
+        <FileUploadButton hidden accept=".conf" ref="uploadRef" @upload="" />
+      </div>
+    </CardContainer>
   </CenteredCardColumn>
 </template>
-import { getSambaManager } from '@/tabs/samba/samba-manager'; import { getServer } from
-'@45drives/houston-common-lib';
