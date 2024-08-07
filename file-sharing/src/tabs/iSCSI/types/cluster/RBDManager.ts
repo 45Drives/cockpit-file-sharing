aa00bf6c553eb5564c88031272f34fc2f7b0f5cd
Brandon Kelly <brkelly@protocase.com>
2024-08-07 15:36:30 -0300

    RBDManager progress

--- file-sharing/src/tabs/iSCSI/types/cluster/RBDManager.ts
+++ file-sharing/src/tabs/iSCSI/types/cluster/RBDManager.ts
@@ -1,11 +1,50 @@
 import type { LogicalVolume } from '@/tabs/iSCSI/types/cluster/LogicalVolume';
-import type { RadosBlockDevice } from './RadosBlockDevice';
-import type { Pool } from "@/tabs/iSCSI/types/cluster/Pool";
+import { RadosBlockDevice } from './RadosBlockDevice';
+import { type Pool } from "@/tabs/iSCSI/types/cluster/Pool";
+import { BashCommand, ProcessError, StringToIntCaster, type Server } from '@45drives/houston-common-lib';
+import { DeviceType } from '@/tabs/iSCSI/types/VirtualDevice';
+import { err, errAsync, ok, okAsync } from 'neverthrow';
 
 export class RBDManager {
 
-    createRadosBlockDevice(name: string, size: string, pool: Pool) {
+    server: Server;
 
+    constructor(server: Server) {
+        this.server = server;
+    }
+
+    createReplicationRadosBlockDevice(name: string, size: string, pool: Pool) {
+        return this.server.execute(new BashCommand(`rbd create ${pool.name}/${name} --size ${size}`))
+        .andThen(() => 
+            this.server.execute(new BashCommand(`rbd map ${pool.name}/${name}`))
+            .andThen((mapProc) => this.server.execute(new BashCommand(`blockdev --getbsz ${mapProc.getStdout()}`))
+                .andThen((blockSizeProc) => {
+                    const blockSize = StringToIntCaster()(blockSizeProc.getStdout())
+
+                    if (!blockSize.isNone())
+                        return okAsync(new RadosBlockDevice(name, mapProc.getStdout(), blockSize.some(), DeviceType.BlockIO, size, pool));
+
+                    return errAsync(new ProcessError("Unable to determine block size of RBD"));
+                })
+            )
+        )
+    }
+
+    createErasureCodedRadosBlockDevice(name: string, size: string, dataPool: Pool, parentPool: Pool) {
+        return this.server.execute(new BashCommand(`rbd create ${parentPool.name}/${name} --size ${size} --data-pool ${dataPool.name}`))
+        .andThen(() => 
+            this.server.execute(new BashCommand(`rbd map ${parentPool.name}/${name}`))
+            .andThen((mapProc) => this.server.execute(new BashCommand(`blockdev --getbsz ${mapProc.getStdout()}`))
+                .andThen((blockSizeProc) => {
+                    const blockSize = StringToIntCaster()(blockSizeProc.getStdout())
+
+                    if (!blockSize.isNone())
+                        return okAsync(new RadosBlockDevice(name, mapProc.getStdout(), blockSize.some(), DeviceType.BlockIO, size, parentPool, dataPool));
+
+                    return errAsync(new ProcessError("Unable to determine block size of RBD"));
+                })
+            )
+        )
     }
 
     createLogicalVolumeFromRadosBlockDevices(rbds: RadosBlockDevice[]) {
--- file-sharing/src/tabs/iSCSI/types/cluster/RadosBlockDevice.ts
+++ file-sharing/src/tabs/iSCSI/types/cluster/RadosBlockDevice.ts
@@ -1,6 +1,16 @@
 import type { Pool } from "@/tabs/iSCSI/types/cluster/Pool";
+import { VirtualDevice, type DeviceType } from "@/tabs/iSCSI/types/VirtualDevice";
 
-export interface RadosBlockDevice {
-    pool: Pool;
+export class RadosBlockDevice extends VirtualDevice {
+    parentPool: Pool;
+    dataPool: Pool | undefined;
     maximumSize: String;
+
+    constructor(deviceName: string, filePath: string, blockSize: number, deviceType: DeviceType, maximumSize: String, parentPool: Pool, dataPool?: Pool) {
+        super(deviceName, filePath, blockSize, deviceType);
+
+        this.parentPool = parentPool;
+        this.dataPool = dataPool;
+        this.maximumSize = maximumSize;
+    }
 }
