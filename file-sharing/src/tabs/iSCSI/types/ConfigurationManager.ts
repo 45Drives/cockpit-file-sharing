31ef184fa9979bcf4738db0101347b57c57fe78b
JordanKeo45 <jkeough@45drives.com>
2025-03-04 11:28:07 -0400

    removes extra commands when creating targets, force loading of config done via service file in playbook now

--- file-sharing/src/tabs/iSCSI/types/ConfigurationManager.ts
+++ file-sharing/src/tabs/iSCSI/types/ConfigurationManager.ts
@@ -26,26 +26,12 @@ export class ConfigurationManager {
             .mapErr(() => new ProcessError("Config file syntax validation failed."))
     }
 
-    // saveCurrentConfiguration(): ResultAsync<File, ProcessError> {
-    //     return new File(this.server, useUserSettings().value.iscsi.confPath)
-    //     .create(true)
-    //     .andThen((file) => this.exportConfiguration().map((config) => file.write(config)).map(() => file))
-    // }
-
     saveCurrentConfiguration(): ResultAsync<File, ProcessError> {
         return new File(this.server, useUserSettings().value.iscsi.confPath)
             .create(true)
             .andThen((file) =>
                 this.exportConfiguration()
                     .map((config) => file.write(config))
-                    // .andThen(() => this.server.execute(new BashCommand(`systemctl enable scst`)))
-                    // .andThen(() => this.server.execute(new BashCommand(`scstadmin -config ${useUserSettings().value.iscsi.confPath} -force -noprompt`)))
-                    // .andThen(() => this.server.execute(new BashCommand(`systemctl restart scst`)))
-                    .andThen(() => this.server.execute(new BashCommand(`
-                        if ! systemctl is-enabled scst &>/dev/null; then
-                            systemctl enable scst
-                        fi
-                    `)))
                     .map(() => file)
             );
     }
