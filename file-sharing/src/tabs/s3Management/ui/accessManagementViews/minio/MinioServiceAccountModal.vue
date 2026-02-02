ab793dfced9ee99af04e34ed646adff294c0465f
hansrachit123 <rhans@protocase.com>
2026-02-02 19:12:02 -0400

    removed title

--- file-sharing/src/tabs/s3Management/ui/accessManagementViews/minio/MinioServiceAccountModal.vue
+++ file-sharing/src/tabs/s3Management/ui/accessManagementViews/minio/MinioServiceAccountModal.vue
@@ -164,7 +164,7 @@
 
                   <div class="relative group">
                     <span class="inline-flex h-5 w-5 items-center justify-center rounded  text-muted"
-                      aria-label="About access key policies" title="About access key policies">
+                      >
                       <!-- Info icon (inline SVG) -->
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-3.5 w-3.5">
